4c78bded46a5dd74e4f76a8272d081074652721d
Megan Kaiser <meganrayanne@yahoo.com>
2018-05-10 11:04:52 -0600

    Set up files

---
+++ test/heap-test.js
@@ -0,0 +1,8 @@
+import { assert } from 'chai';
+import Heap from '../lib/heap.js';
+
+describe('Heap', function () {
+  it('should return true', function () {
+    assert.equal(true, true);
+  });
+});
